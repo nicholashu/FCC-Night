8c7add23d21ec1289a6f90bb657a053d6ec396db
Nick Hu <nick@nickhu.info>
2016-02-02 10:58:35 +0800

    yelp functional

--- app/controllers/barController.client.js
+++ app/controllers/barController.client.js
@@ -1,23 +1,23 @@
 'use strict';
 
-(function () {
-	angular
-		.module('BarController', ['clementineBarApp'])
-		.controller('BarCtrl',
-			['$scope',
-			'$resource',
-			function ($scope, $resource) {
-				  var yelpUrl = appUrl + '/api/yelp/';
-					var searchLocation = "Perth";
+(function() {
+    angular
+      .module('BarController', ['clementineBarApp'])
+      .controller('BarCtrl', ['$scope',
+        '$resource', '$window', '$http',
+        function($scope, $resource, $window, $http) {
+					var appUrl = $window.location.origin;
+          var yelpUrl = appUrl + '/api/yelp/';
+          var searchLocation = "Perth";
 
-				function = getBars (){
-                $http.get(yelpUrl + searchLocation).then(function(response) {
-                  console.log(response);
-                });
-              }
-            };
-				}
+          function getBars() {
+            $http.get(yelpUrl + searchLocation).then(function(response) {
+              console.log(response.data.businesses);
+            });
+          }
+          getBars();
 
 
-		}]);
+
+      }]);
 })();
