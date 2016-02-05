46ac033e0da3c627dfd42a50db074a0c98b364d2
Nick Hu <nick@nickhu.info>
2016-02-05 14:38:35 +0800

    logged in check

--- app/routes/index.js
+++ app/routes/index.js
@@ -49,18 +49,6 @@ app.route('/')
     });
 
 
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect: '/', // redirect to the secure profile section
-    failureRedirect: '/login', // redirect back to the signup page if there is an error
-    failureFlash: true // allow flash messages
-  }));
-
-  app.post('/login',
-    passport.authenticate('local-login'),
-    function(req, res) {
-      res.redirect('/' + req.user);
-    });
-
   app.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/', // redirect to the secure profile section
     failureRedirect: '/signup', // redirect back to the signup page if there is an error
