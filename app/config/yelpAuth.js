3d87bab8712ada1f63b3c215b3a05750a57d196f
Nick Hu <nick@nickhu.info>
2016-02-19 12:02:15 +0800

    yelpAuth

---
+++ app/config/yelpAuth.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+	'yelpAuth' : {
+		'consumerKey'       : process.env.YELP_CONSUMER_KEY,
+		'consumerSecret'    : process.env.YELP_CONSUMER_SECRET,
+		'token' 						: process.env.YELP_TOKEN,
+		'tokenSecret'				:	process.env.YELP_TOKEN_SECRET
+},
+};
